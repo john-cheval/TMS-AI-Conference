7f2b1315335df71e432c4eec5929f82eb36b63d6
john-cheval <johncheval1@gmail.com>
2025-09-04 13:12:28 +0400

    footer links are done

--- src/components/shared/ui/Headings/SectionHeading/index.tsx
+++ src/components/shared/ui/Headings/SectionHeading/index.tsx
@@ -2,11 +2,21 @@ import React from "react";
 
 export type SectionHeadingPropsTypes = {
   main_title?: string;
+  isDark?: boolean;
+  isCenter?: boolean;
 };
-const SectionHeading = ({ main_title }: SectionHeadingPropsTypes) => {
+const SectionHeading = ({
+  main_title,
+  isDark = false,
+  isCenter = false,
+}: SectionHeadingPropsTypes) => {
   return (
     <h3
-      className="home-about-heading  main-heading text-tms-black font-bold leading-3 lg:leading-2 xl:leading-1 "
+      className={`home-about-heading ${
+        isCenter ? "text-center lg:text-left md:!leading-2 " : ""
+      }  main-heading ${
+        isDark ? "text-white" : "text-tms-black"
+      } font-bold leading-3 lg:leading-2 xl:leading-1`}
       dangerouslySetInnerHTML={{ __html: main_title ?? "" }}
     ></h3>
   );
