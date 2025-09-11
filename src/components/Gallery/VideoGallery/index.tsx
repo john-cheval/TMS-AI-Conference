18665a6846a77448c68c790cd6b374c4b2f3cfef
john-cheval <johncheval1@gmail.com>
2025-09-11 16:54:33 +0400

    pre-release page has been done

--- src/components/Gallery/VideoGallery/index.tsx
+++ src/components/Gallery/VideoGallery/index.tsx
@@ -1,36 +1,28 @@
 "use client";
+import VideoCard from "@/components/shared/ui/Cards/VideoCard";
+import { getYoutubeId } from "@/utils/getYouyubeId";
 import React, { useState } from "react";
-import GalleryCard from "@/components/shared/ui/Cards/GalleryCard";
 
-export interface PhotoGalleryType {
+interface videoGalleryType {
   id: number;
-  award_id: number;
-  photo_order: number;
-  main_image: string | null;
   title: string;
-  slug: string;
-  sponsers_id: number | null;
-  status: string;
-  image: string;
-  created_at: string;
-  updated_at: string;
-  image_url: string;
+  link: string;
 }
-
 interface GalleryType {
   id: number;
   name: string;
   year: string;
-  photo_gallery: PhotoGalleryType[];
+  video_gallery: videoGalleryType[];
 }
 
 type Props = {
   data: GalleryType[];
 };
 
-const GalleryAlbums = ({ data }: Props) => {
+const VideoGallery = ({ data }: Props) => {
   const [selectedConferece, setSelectedConference] = useState(data[0]?.name);
   const filteredAlbum = data?.find((item) => item?.name === selectedConferece);
+  // const [activeVideo, setActiveVideo] = useState<number | null>(null);
   return (
     <section className="section-wrapper pt-6 md:pt-8 lg:pt-10 xl:pt-14 pb-8 md:pb-12 lg:pb-16 xl:pb-20">
       <div className="flex gap-x-2.5 md:gap-x-3 lg:justify-center  overflow-x-auto whitespace-nowrap no-scrollbar w-full flex-nowrap">
@@ -70,16 +62,25 @@ const GalleryAlbums = ({ data }: Props) => {
             );
           })}
       </div>
-
       <div className="mt-6 md:mt-8 lg:mt-10 xl:mt-12 grid grid-cols-2 md:grid-cols-3 3xl:grid-cols-4 gap-2.5 md:gap-3 lg:gap-4">
-        {Array.isArray(filteredAlbum?.photo_gallery) &&
-        filteredAlbum.photo_gallery.length > 0 ? (
-          filteredAlbum?.photo_gallery?.map((item, index) => {
-            return <GalleryCard key={index + 1} {...item} isAlbumPage={true} />;
+        {Array.isArray(filteredAlbum?.video_gallery) &&
+        filteredAlbum.video_gallery.length > 0 ? (
+          filteredAlbum?.video_gallery?.map((item, index) => {
+            const youtubeId = getYoutubeId(item.link);
+            return (
+              <VideoCard
+                key={index + 1}
+                title={item.title}
+                link={item.link}
+                youtube_id={youtubeId ?? ""}
+                // onPlay={() => setActiveVideo(index)}
+                // isActive={activeVideo === index}
+              />
+            );
           })
         ) : (
           <p className="text-lg sm:text-xl md:text-2xl col-span-12 font-medium text-center">
-            There is No Albums in this Conference Year
+            There is No Video in this Conference Year
           </p>
         )}
       </div>
@@ -87,4 +88,4 @@ const GalleryAlbums = ({ data }: Props) => {
   );
 };
 
-export default GalleryAlbums;
+export default VideoGallery;
