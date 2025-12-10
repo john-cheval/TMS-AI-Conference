5e037b1c6f48b3acec894cb9cc1daca095df860c
john-cheval <johncheval1@gmail.com>
2025-12-10 14:48:41 +0530

    pricing plan show

--- src/components/DelegateRegistration/Plans/index.tsx
+++ src/components/DelegateRegistration/Plans/index.tsx
@@ -17,17 +17,21 @@ const PricingPlans = ({
   console.log("companyList",companyList)
   return (
     <section className="pt-5 md:pt-8 lg:pt-12  xl:pt-16 2xl:pt-20 section-wrapper">
-      {/* <div>
-        <h2 className="main-heading-2 gradient-text  md:mx-auto w-fit !leading-2 mb-3 md:mb-5 xl:mb-8">
-          {heading}
-        </h2>
+      {
+        !isFree ? 
+        <div>
+          <h2 className="main-heading-2 gradient-text  md:mx-auto w-fit !leading-2 mb-3 md:mb-5 xl:mb-8">
+            {heading}
+          </h2>
 
-        <PlansTable
-          data={price_list}
-          heading_1={table_heading}
-          heading_2={table_heading_2}
-        />
-      </div> */}
+          <PlansTable
+            data={price_list}
+            heading_1={table_heading}
+            heading_2={table_heading_2}
+          />
+        </div>
+        : ""
+      }
 
       <DelegateRegisterForm
         heading={register_heading}
