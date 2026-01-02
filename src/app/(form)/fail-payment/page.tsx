5f7f8a06760440e408cf2aa2c475927006806fdc
john-cheval <johncheval1@gmail.com>
2026-01-02 18:50:48 +0530

    Payment integration

--- src/app/(form)/fail-payment/page.tsx
+++ src/app/(form)/fail-payment/page.tsx
@@ -13,11 +13,12 @@ async function FailPayment({ searchParams}:any) {
     return (
         <>
             {
-            pageContent?.payment_details.state === 'FAILED' && (
+            // pageContent?.payment_details?.state === 'FAILED' && (
                 <div className='thank-you container-inside mx-auto py-[50px]'>
-                    <h1 className='text-center text-[#0078bb] text-[40px] font-[700]'>{pageContent?.msg}</h1>
+                    {/* <h1 className='text-center text-[#0078bb] text-[40px] font-[700]'>{pageContent?.msg}</h1> */}
+                    <h1 className='text-center text-[#0078bb] text-[40px] font-[700]'>Your payment is failed.</h1>
                 </div>
-            ) 
+            // ) 
             }
         </>
     );
